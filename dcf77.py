a656643030c04ea02ec6c1bcec9d545fbe028e41
ivanbekker22 <40799347+ivanbekker22@users.noreply.github.com>
2025-11-07 15:49:25 +0700

    Add files via upload

--- dcf77.py
+++ dcf77.py
@@ -1,17 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-n = np.arange(0, 12000000, 1)
-length = np.size(n)
-imp0 = np.zeros(length)
-def impl0():
-    val = np.where((n >= 20000) & (n <= 200000))
-    imp0[val] = 1
-    return imp0
+n = np.arange(0, 12e5, 1)
+minute_sample = np.size(n)
+imp = np.ones(minute_sample)
 
+rand_bin = np.random.randint(0, 2, size=1)
 
+step = 0
+i = 0
+fs = 2e5
 
-plt.plot(n, imp0)
+def null_impulse(bit):
+    if bit == 1:
+        imp[0:10000] = 0
+        imp[200000:220000] = 0
+    if bit == 0:
+        imp[200000:220000] = 0
+        imp[0:10000] = 0
+    return imp
+
+null_impulse(bit = rand_bin[0])
+
+plt.plot(n, imp)
 plt.grid(True)
 plt.show()
 
